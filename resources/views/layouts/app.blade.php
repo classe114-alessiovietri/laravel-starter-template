f87f56614c8fbb8ff1a9705d8f30c8337b71fc5a
alessiovietri <info@alessiovietri.it>
2024-02-23 12:01:18 +0100

    Completato LC iniziale

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>@yield('page-title') | Laravel Start 1</title>
+
+        {{-- Stili nell'head --}}
+        <link rel="stylesheet" href="css/style.css">
+        @yield('head-imports')
+    </head>
+    <body>
+
+        @include('partials.header')
+
+        <main>
+            @yield('main-content')
+        </main>
+
+        @include('partials.footer')
+
+        {{-- JS da importare nel body --}}
+        @yield('body-imports')
+    </body>
+</html>
