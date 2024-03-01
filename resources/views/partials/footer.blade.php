30b587c7365ce795708c34259e8ba7ee025f435d
alessiovietri <info@alessiovietri.it>
2024-03-01 09:28:04 +0100

    Aggiornato footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,11 @@
 <footer>
-    Footer
+    <div class="container">
+        <div class="row">
+            @for ($i = 1; $i <= 4; $i++)
+                <div class="col-12 col-sm-6 col-md-3">
+                    Footer {{ $i }}
+                </div>
+            @endfor
+        </div>
+    </div>
 </footer>
